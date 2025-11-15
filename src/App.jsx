4ca84cadf3ef0a2b2540ae335730609681eebd27
ashwin335 <b21ec037@gmail.com>
2025-11-15 20:34:44 +0530

    second commit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import UserInfo from "./Userinfo";
 export default function App() {
   return (
     <div>
+      <p>Welcome to the React Redux project</p>
       <UserInfo/>
       <Button/>
       
